72462424b669f1bd3d8719f6ce3e473b11a17bb7
AimeexJB <aimeexjb@gmail.com>
2019-03-05 15:08:15 +0000

    getting there

--- script2.js
+++ script2.js
@@ -1,56 +1,63 @@
+var canvas, engine;
+var scene, camera, createScene;
+
 window.addEventListener('DOMContentLoaded', function(){
 
-  var canvas = document.getElementById('renderCanvas');
-  var engine = new BABYLON.Engine(canvas, true);
+    canvas = document.getElementById('renderCanvas');
+    engine = new BABYLON.Engine(canvas, true);
 
-  var createScene = function(){
+    function createScene(){
 
-    var scene = new BABYLON.Scene(engine);
-    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
+        //--------------------------Creating Scene--------------------------//
+        scene = new BABYLON.Scene(engine);
+        camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
 
-    camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl(canvas, true);
+        camera.setTarget(BABYLON.Vector3.Zero());
+        camera.attachControl(canvas, true);
 
-    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+        //--------------------------Adding text to screen--------------------------//
+        container = document.createElement( 'div' );
+    	document.body.appendChild( container );
 
-    for( var i = 0; i < 300; i++) {
-      var box = BABYLON.Mesh.CreateBox('box1', 1, scene);
-      box.position.x = (Math.random()* 40) - 20;
-      box.position.y = (Math.random()* 40) - 20;
-      box.position.z = (Math.random()* 40) - 20;
+    	var info = document.createElement( 'div' );
+        	info.style.position = 'absolute';
+        	info.style.top = '20px';
+        	info.style.color = 'white'
+        	info.style.width = '100%';
+        	info.style.textAlign = 'center';
+        	info.innerHTML = 'Interactive Museum';
+    	container.appendChild( info );
+
+        //--------------------------Adding The lighting effects--------------------------//
+        var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+
+        //--------------------------Adding the Cubes to the Scene--------------------------//
+        for( var i = 0; i < 300; i++) {
+            var box = BABYLON.Mesh.CreateBox('box1', 1, scene);
+            box.position.x = (Math.random()* 40) - 20;
+            box.position.y = (Math.random()* 40) - 20;
+            box.position.z = (Math.random()* 40) - 20;
+        }
+        box.isPickable = true;
+
+        //--------------------------Loading the Background Image--------------------------//
+        var mat = new BABYLON.StandardMaterial("contain", scene);
+            mat.diffuseTexture = new BABYLON.Texture("public/grid.png", scene);
+            mat.backFaceCulling = false;
+        var containBox = BABYLON.Mesh.CreateBox("box2", 200.0, scene);
+            containBox.material = mat;
+
+        return scene;
     }
 
-    // var mat = new BABYLON.StandardMaterial("contain", scene);
-    // mat.diffuseTexture = new BABYLON.Texture("public/3.png", scene);
-    // mat.backFaceCulling = false;
-    // var containBox = BABYLON.Mesh.CreateBox("box2", 200.0, scene);
-    // containBox.material = mat;
+    scene = createScene();
 
-    var skybox = BABYLON.Mesh.CreateBox("skyBox", 100.0, scene);
-    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-    skyboxMaterial.backFaceCulling = false;
-    skyboxMaterial.disableLighting = true;
-    skybox.infiniteDistance = true;
-    skyboxMaterial.disableLighting = true;
-    skybox.material = skyboxMaterial;
+    engine.runRenderLoop(function(){
+        scene.render();
+    });
 
-    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("public/3", scene);
-    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-
-    skybox.renderingGroupId = 0;
-    box.renderingGroupId = 1;
-
-    return scene;
-  }
-
-  var scene = createScene();
-
-  engine.runRenderLoop(function(){
-    scene.render();
-  });
-
-  window.addEventListener('resize', function(){
-    engine.resize();
-  });
+    window.addEventListener('resize', function(){
+        engine.resize();
+    });
 
 });
