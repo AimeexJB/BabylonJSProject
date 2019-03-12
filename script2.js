aad858e171d2efcfe84ed77eec1cc354babbcd1d
Aimee Redmond <32738835+AimeexJB@users.noreply.github.com>
2019-03-12 18:01:22 +0000

    added music, styling and fixes to div

--- script2.js
+++ script2.js
@@ -5,6 +5,28 @@ var  mouse, INTERSECTED, raycaster, selectedObject;
 
 var objects = [];
 
+var songs = [{"title": "Smells Like Teen Spirit - Nirvana", "source": "music/SmellsLikeTeenSpirit.mp3", "description": "10/10 would listen to"},
+			{"title": "Imagine - John Lennon", "source": "music/Imagine.mp3", "description": "10/10 would listen to"},
+			{"title": "One - U2", "source": "music/One.mp3", "description": "10/10 would listen to"},
+			{"title": "Billie Jean - Michael Jackson", "source": "music/BillieJean.mp3", "description": "10/10 would listen to"},
+			{"title": "Bohemian Rhapsody - Queen", "source": "music/BohemianRhapsody.mp3", "description": "10/10 would listen to"},
+			{"title": "Hey Jude - The Beatles", "source": "music/HeyJude.mp3", "description": "10/10 would listen to"},
+			{"title": "I Can't Get No Satisfaction - Rolling Stones", "source": "music/ICantGetNo.mp3", "description": "10/10 would listen to"},
+			{"title": "Sweet Child O'Mine - Guns N' Roses", "source": "music/SweetChildMine.mp3", "description": "10/10 would listen to"},
+			{"title": "London Calling - The Clash", "source": "music/LondonCalling.mp3", "description": "10/10 would listen to"},
+			{"title": "Hotel California - The Eagles", "source": "music/HotelCalifornia.mp3", "description": "10/10 would listen to"},
+			{"title": "Your Song - Elton John", "source": "music/YourSong.mp3", "description": "10/10 would listen to"},
+			{"title": "Stairway To Heaven - Led Zeppelin", "source": "music/StairwayToHeaven.mp3", "description": "Song from Your Name movie 10/10 would listen to"},
+			{"title": "I Will Always Love You - Whitney Houston", "source": "music/IWillAlwaysLoveYou.mp3", "description": "Song from Your Name movie 10/10 would listen to"},
+			{"title": "Heartbreak Hotel - Elvis Presley", "source": "music/HeartbreakHotel.mp3", "description": "Song from Your Name movie 10/10 would listen to"},
+			{"title": "Over The Rainbow - Judy Garland", "source": "music/OverTheRainbow.mp3", "description": "Song from Your Name movie 10/10 would listen to"},
+			{"title": "What's Goin' On - Marvin Gaye", "source": "music/WhatsGoinOn.mp3", "description": "Song from Your Name movie 0/10 would listen to"},
+			{"title": "Creep - Radiohead", "source": "music/Creep.mp3", "description": "Song from Your Name movie 6/10 would listen to"},
+			{"title": "Bridge Over Troubled Water - Simon & Garfunkel", "source": "music/BridgeOverTroubledWater.mp3", "description": "Song from Your Name movie 6/10 would listen to"},
+			{"title": "Respect - Aretha Franklin", "source": "music/Respect.mp3", "description": "Song from Your Name movie 6/10 would listen to"},
+			{"title": "Dancing Queen - ABBA", "source": "music/DancingQueen.mp3", "description": "Song from Your Name movie 6/10 would listen to"}];
+
+
 window.addEventListener('DOMContentLoaded', function(){
 
     canvas = document.getElementById('renderCanvas');
@@ -36,7 +58,7 @@ window.addEventListener('DOMContentLoaded', function(){
         var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
         //--------------------------Adding the Cubes to the Scene--------------------------//
-        for( var i = 0; i < 20; i++) {
+        for( var i = 0; i < 10; i++) {
             box = BABYLON.Mesh.CreateBox('box1', 1, scene);
             box.position.x = (Math.random()* 40) - 20;
             box.position.y = (Math.random()* 40) - 20;
@@ -45,43 +67,40 @@ window.addEventListener('DOMContentLoaded', function(){
             objects.push( box );
         }
 
-        // box.isPickable = true;
+        box.isPickable = true;
 
         var mat = new BABYLON.StandardMaterial("mat", scene);
         box.material = mat;
 
-        //--------------------------Making box selectable--------------------------//
-
         var actionManager = new BABYLON.ActionManager(scene);
         box.actionManager = actionManager;
-        
-        actionManager.registerAction(
-            new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger,
-                () => mat.diffuseColor = BABYLON.Color3.Random()
-            )
-        );
+
+        //--------------------------changing box colour--------------------------//
+        // actionManager.registerAction(
+        //     new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger,
+        //         () => mat.diffuseColor = BABYLON.Color3.Random()
+        //     )
+        // );
 
         //--------------------------adding in DIVs--------------------------//
+        var boxinfo = document.createElement( 'div' );
+		var songTitle = document.createElement( 'h3' );
+		var songInfo = document.createElement( 'p' );
+			boxinfo.appendChild( songTitle );
+			boxinfo.appendChild( songInfo );
+			boxinfo.setAttribute("class", "modal");
+			boxinfo.style.color = 'black'
+            boxinfo.style.display = "none";
 
-        var div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
-        div.style.position = "absolute";
-        div.style.width = "200px";
-        div.style.height = "200px";
-        div.style.top = "50%";
-        div.style.left = "50%";
-        div.style.transform = "translate(-50%, -50%)";
-        div.style.display = "none";
-        document.body.appendChild(div);
+		document.body.appendChild( boxinfo );
 
-        div.addEventListener("click", () => div.style.display = "none");
+        boxinfo.addEventListener("click", () => boxinfo.style.display = "none");
 
-        var actionSphere = new BABYLON.ActionManager(scene);
-        box.actionManager = actionSphere;
+        boxinfo.innerHTML = box.id;
 
-        actionSphere.registerAction(
+        actionManager.registerAction(
             new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger,
-                () => div.style.display = "block"
+                () => boxinfo.style.display = "block",
             )
         );
 
